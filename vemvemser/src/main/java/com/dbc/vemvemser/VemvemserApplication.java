ebb77ec5e12e16726320b4936cc3ab808a526112
Edyfiva <edyfiva@gmail.com>
2022-11-25 15:53:03 -0300

    projeto

---
+++ vemvemser/src/main/java/com/dbc/vemvemser/VemvemserApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.vemvemser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemvemserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemvemserApplication.class, args);
+	}
+
+}
