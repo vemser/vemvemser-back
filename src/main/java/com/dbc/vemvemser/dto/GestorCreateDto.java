5351e24c62c01b4cddc376353db31d81219d87ee
Edyfiva <edyfiva@gmail.com>
2022-11-26 02:05:45 -0300

    correções rapidas

--- src/main/java/com/dbc/vemvemser/dto/GestorCreateDto.java
+++ src/main/java/com/dbc/vemvemser/dto/GestorCreateDto.java
@@ -1,4 +1,13 @@
 package com.dbc.vemvemser.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GestorCreateDto {
+
+    private String nome;
 }
--- src/main/java/com/dbc/vemvemser/entity/CandidatoEntity.java
+++ src/main/java/com/dbc/vemvemser/entity/CandidatoEntity.java
@@ -3,7 +3,6 @@ package com.dbc.vemvemser.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
