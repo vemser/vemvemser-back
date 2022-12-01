1f309c1c89014c4abca709adad985df185a0fd87
Edyfiva <edyfiva@gmail.com>
2022-12-01 03:18:38 -0300

    teste candidato

---
+++ src/test/java/com/dbc/vemvemser/AvaliacaoServiceTest.java
@@ -0,0 +1,36 @@
+package com.dbc.vemvemser;
+
+
+import com.dbc.vemvemser.repository.AvaliacaoRepository;
+import com.dbc.vemvemser.service.AvaliacaoService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AvaliacaoServiceTest {
+
+    @InjectMocks
+    private AvaliacaoService avaliacaoService;
+
+    @Mock
+    private AvaliacaoRepository avaliacaoRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Before
+    public void init() {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        ReflectionTestUtils.setField(avaliacaoService, "objectMapper", objectMapper);
+    }
+}
---
+++ src/test/java/com/dbc/vemvemser/EmailServiceTest.java
@@ -0,0 +1,14 @@
+package com.dbc.vemvemser;
+
+import com.dbc.vemvemser.service.EmailService;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+
+    @InjectMocks
+    private EmailService emailService;
+
+}
